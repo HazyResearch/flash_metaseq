acb096a981a7472485d45c284b1c6da8667d37fc
root <zeighami@usc.edu>
2022-06-07 17:49:28 -0700

    run 350m

---
+++ get_test.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from flask import jsonify
+
+url = 'http://127.0.0.1:6010/completions'
+headers = {'Content-Type': 'application/json'}
+
+filters = {'prompt':'LinkedIn is a great company2'}
+#print(jsonify(filters))
+#params = dict(json=json.dumps(filters))
+print(filters)
+print(json.dumps(filters))
+response = requests.post(url, json=filters,headers=headers)
+#print(response.text)
+assert response.status_code == 200
+print(response.json()['choices'][0]['text'])
+print(len(response.json()['choices']))
+
+exit()
+
+import requests
+
+url = 'http://127.0.0.1:6010'
+#url = 'https://httpbin.org/post'
+
+# POST/form data
+payload = {
+    'search': 'hello world',
+}
+
+r = requests.post(url, data=payload)
+
+print(r.text)
